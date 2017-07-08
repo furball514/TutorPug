2689c0215febda477c03b386f866d3dee8de3dd6
furball514 <karthik.nallasivan@gmail.com>
2017-07-08 16:49:24 +0530

    4:45

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Sentry from "sentry-expo";
+Sentry.config(
+  "https://a91c8897643140638ddfd686dbf42476@sentry.io/189285"
+).install();
 
 export default class App extends React.Component {
   render() {
