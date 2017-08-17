676afe79d38cb4b4e27490d55b1bb1bbf731ae8a
furball514 <karthik.nallasivan@gmail.com>
2017-08-17 20:23:49 +0530

    8:23

--- src/authenticated/forms/studentform.js
+++ src/authenticated/forms/studentform.js
@@ -5,13 +5,14 @@ import {
   TextInput,
   StyleSheet,
   Platform,
-  KeyboardAvoidingView,
+  ScrollView,
   TouchableOpacity
 } from "react-native";
 
 export default class StudentForm extends React.Component {
   state = {
-    firstName: "jared"
+    firstName: "",
+    lastName: ""
   };
 
   static navigationOptions = {
@@ -24,9 +25,9 @@ export default class StudentForm extends React.Component {
               style={{
                 color: "#5856d6",
                 fontWeight: "600",
-                marginRight: 15,
+                marginRight: 13,
                 marginTop: 3,
-                fontSize: 17
+                fontSize: 19
               }}
             >
               Done
@@ -35,9 +36,17 @@ export default class StudentForm extends React.Component {
         : null
   };
 
+  componentWillMount() {
+    const { params } = this.props.navigation.state;
+    this.setState({
+      firstName: params.data.firstName,
+      lastName: params.data.lastName
+    });
+  }
+
   render() {
     return (
-      <KeyboardAvoidingView behavior="padding">
+      <ScrollView>
         <View style={[styles.border, { marginTop: 40 }]} />
         <View style={styles.section}>
           <Text
@@ -50,6 +59,9 @@ export default class StudentForm extends React.Component {
           <Text allowFontScaling={false}> gty </Text>
         </View>
         <View style={[styles.border, { marginBottom: 40 }]} />
+        <Text allowFontScaling={false} selectable={false} style={styles.title}>
+          BASIC INFO
+        </Text>
         <View style={styles.border} />
         <View style={styles.section}>
           <Text
@@ -62,11 +74,38 @@ export default class StudentForm extends React.Component {
           <TextInput
             style={styles.textInput}
             value={this.state.firstName}
+            autoCapitalize="words"
+            maxLength={40}
+            placeholder="required"
+            placeholderTextColor="#8a8a92"
+            returnKeyType="next"
+            selectionColor="#FDF760"
             onChangeText={firstName => this.setState({ firstName })}
           />
         </View>
         <View style={styles.border} />
-      </KeyboardAvoidingView>
+        <View style={styles.section}>
+          <Text
+            selectable={false}
+            allowFontScaling={false}
+            style={styles.label}
+          >
+            Last Name
+          </Text>
+          <TextInput
+            style={styles.textInput}
+            value={this.state.lastName}
+            autoCapitalize="words"
+            maxLength={40}
+            placeholder="required"
+            placeholderTextColor="#8a8a92"
+            returnKeyType="next"
+            selectionColor="#FDF760"
+            onChangeText={lastName => this.setState({ lastName })}
+          />
+        </View>
+        <View style={styles.border} />
+      </ScrollView>
     );
   }
 }
@@ -75,19 +114,31 @@ const styles = StyleSheet.create({
   section: {
     flex: 1,
     flexDirection: "row",
-    justifyContent: "space-around", //
-    height: 65, //
+    justifyContent: "space-between",
+    height: 65,
     alignItems: "center"
   },
   label: {
     fontWeight: "500",
     color: "#0F5A43",
-    fontSize: 24
+    fontSize: 24,
+    marginLeft: 20
+  },
+  title: {
+    color: "#8a8a92",
+    fontSize: 16,
+    fontWeight: "400",
+    marginBottom: 5,
+    marginLeft: 20
   },
   textInput: {
-    color: "#FDF760",
     fontFamily: "roboto",
-    height: 20
+    textAlign: "right",
+    fontSize: 22,
+    height: 40,
+    width: 140,
+    alignSelf: "center",
+    marginRight: 20
   },
   border: {
     borderBottomWidth: 0.3,
@@ -95,6 +146,7 @@ const styles = StyleSheet.create({
   }
 });
 
+//focus
 //style
 //view
 //inputs , defaults
